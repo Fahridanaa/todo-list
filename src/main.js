4accefd2d97eea308c5a30018797ce44fd117364
Fahridanaa <fahridanaaa@gmail.com>
2023-09-26 22:50:05 +0700

    boh mumet

--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
-import Card from './Card';
 import createSidebar from './sidebar';
 
-export default () => {
+export default function createMain() {
   const todoData = JSON.parse(localStorage.getItem('todoData')) || [];
   const main = document.createElement('main');
   main.setAttribute('id', 'main');
 
+  const sidebar = createSidebar();
+
   let cards = document.createElement('div');
   cards.setAttribute('id', 'cards');
-  
-  todoData.forEach((todo, index) => {
-    const card = new Card(todo, index);
-    cards.appendChild(card.element);
-  });
 
-  main.appendChild(createSidebar());
+  main.appendChild(sidebar);
   main.appendChild(cards);
   return main;
 }
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,3 +1,27 @@
+import isToday from "date-fns/isToday";
+import Card from "./Card";
+
+function filterTodayTodo(todoList) {
+  if(todoList === undefined || todoList.length == 0) { return; }
+  return todoList.filter(todo => isToday(todo.dueDate));
+}
+
+function filterTodo(isToday) {
+  const cards = document.getElementById('cards');
+  cards.innerHTML = '';
+
+  const todoData = JSON.parse(localStorage.getItem('todoData')) || [];
+  const filteredTodo = isToday
+    ? filterTodayTodo(todoData)
+    : todoData;
+
+  filteredTodo.forEach((todo, index) => {
+    const card = new Card(todo, index);
+    cards.appendChild(card.element);
+  });
+}
+
+
 function createDashboardButton(text) {
   const button = document.createElement('button');
   button.innerHTML = text;
@@ -10,10 +34,19 @@ function createDashboardSection() {
 
   const dashboardContent = ['Dashboard', 'Today'];
 
-  dashboardContent.forEach(text => {
-    const button = createDashboardButton(text);
-    dashboardSection.appendChild(button);
-  });
+  for (let index = 0; index < dashboardContent.length; index++) {
+    let btn = createDashboardButton(dashboardContent[index]);
+    dashboardSection.appendChild(btn);
+
+    // Tambahkan event listener untuk setiap tombol
+    btn.addEventListener('click', () => {
+      if (index === 0) {
+        filterTodo(false);
+      }else {
+        filterTodo(true);
+      }
+    });
+  }
 
   return dashboardSection;
 }
